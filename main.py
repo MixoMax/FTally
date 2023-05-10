8c9b104c09a88d806b369f1a80c656824509a61c
Linus Horn <linushorn@gmx.net>
2023-05-10 20:02:09 +0200

    completely working with SQLAlchemy

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 import os
 import json
 import time
 import csv
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///counter.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.app_context().push()
+db = SQLAlchemy(app)
+
 
 
 class Counter():
-    def __init__(self, t_start, db_path = "database.db", source = "Dev"):
+    def __init__(self, t_start, db, source = "Dev-Test"):
         
-        self.source = source
-        
-        self.count = 0
-        self.t_start = t_start
-        
-        self.total_plus = 0
-        self.total_minus = 0
-        
-        
-        
-        if os.path.exists(db_path):
-            self.db = Database(db_path)
-            print("Database exists")
-            self.count = self.db.cur.execute("SELECT count FROM counter ORDER BY id DESC LIMIT 1").fetchone()[0]
-            self.total_plus = self.db.cur.execute("SELECT total_plus FROM counter ORDER BY id DESC LIMIT 1").fetchone()[0]
-            self.total_minus = self.db.cur.execute("SELECT total_minus FROM counter ORDER BY id DESC LIMIT 1").fetchone()[0]
-            self.t_start = self.db.cur.execute("SELECT timestamp FROM counter ORDER BY id ASC LIMIT 1").fetchone()[0]
-            print(self.t_start)
+        latest = db.get_last()
+        if latest != False:
+            self.count, self.t_start, timestamp_rel, self.source, self.total_plus, self.total_minus = latest
         else:
-            self.db = Database(db_path)
-            self.db.register(self.count, self.t_start, 0, self.source, self.total_plus, self.total_minus)
+            self.count = 0
+            self.t_start = t_start
+            self.source = source
+            self.total_plus = 0
+            self.total_minus = 0
+        self.db = db
+    
     
     def add(self):
         self.count += 1
@@ -52,60 +49,74 @@ class Counter():
     def export_csv(self):
         self.db.export_csv()
 
+class DB_entry(db.Model):
+    _id = db.Column("id", db.Integer, primary_key = True)
+    count = db.Column("count", db.Integer)
+    timestamp = db.Column("timestamp", db.Float)
+    timestamp_rel = db.Column("timestamp_rel", db.Float)
+    source = db.Column("source", db.String(100))
+    total_plus = db.Column("total_plus", db.Integer)
+    total_minus = db.Column("total_minus", db.Integer)
+    
+    def __init__(self, count, timestamp, timestamp_rel, source, total_plus, total_minus):
+        self.count = count
+        self.timestamp = timestamp
+        self.timestamp_rel = timestamp_rel
+        self.source = source
+        self.total_plus = total_plus
+        self.total_minus = total_minus
 
-class Database():
-    def __init__(self, path = "database.db"):
-        self.path = path
-        self.conn = sqlite3.connect(self.path)
-        self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS counter (id INTEGER PRIMARY KEY, count INTEGER, timestamp float, timestamp_rel float, source TEXT, total_plus INTEGER, total_minus INTEGER)")
-        self.conn.commit()
+class DB():
+    def __init__(self):
+        self.db = db
+        self.db.create_all()
     
     def register(self, count, timestamp, timestamp_rel, source, total_plus, total_minus):
-        self.cur.execute("INSERT INTO counter VALUES (NULL, ?, ?, ?, ?, ?, ?)", (count, timestamp, timestamp_rel, source, total_plus, total_minus))
-        self.conn.commit()
+        entry = DB_entry(count, timestamp, timestamp_rel, source, total_plus, total_minus)
+        self.db.session.add(entry)
+        self.db.session.commit()
+    
+    def get_last(self):
+        if DB_entry.query.count() == 0:
+            return False
+        entry = DB_entry.query.order_by(DB_entry._id.desc()).first()
+        return [entry.count, entry.timestamp, entry.timestamp_rel, entry.source, entry.total_plus, entry.total_minus]
     
     def export_csv(self):
-        """Export the database to a csv file"""
-        
-        csv_path = self.path.split(".")[0] + ".csv"
-        
-        with open(csv_path, "w", newline="") as f:
+        entries = DB_entry.query.all()
+        with open("export.csv", "w", newline = "") as f:
             writer = csv.writer(f)
             writer.writerow(["id", "count", "timestamp", "timestamp_rel", "source", "total_plus", "total_minus"])
-            for row in self.cur.execute("SELECT * FROM counter"):
-                writer.writerow(row)
-    
+            for entry in entries:
+                writer.writerow([entry._id, entry.count, entry.timestamp, entry.timestamp_rel, entry.source, entry.total_plus, entry.total_minus])
 
-class Flask_server():
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.counter = Counter(time.time(), source = "Flask", db_path = "database.db")
-        
-        @self.app.route("/")
-        def index():
-            return render_template("index.html", count = self.counter.count)
-        
-        @self.app.route("/script.js")
-        def script():
-            return url_for("static", filename="script.js")
-        
-        @self.app.route("/add", methods=["POST"])
-        def add():
-            self.counter.add()
-            return self.counter.count, 200
-        
-        @self.app.route("/sub", methods=["POST"])
-        def sub():
-            self.counter.sub()
-            return self.counter.count, 200
-        
-        @self.app.route("/csv", methods=["GET"])
-        def csv():
-            self.counter.export_csv()
-            return url_for("static", filename="database.csv"), 200
+
+counter = Counter(time.time(), DB())
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/add", methods = ["POST"])
+def add():
+    counter.add()
+    return "OK", 204
+
+@app.route("/sub", methods = ["POST"])
+def sub():
+    counter.sub()
+    return "OK", 204
+
+@app.route("/export", methods = ["POST"])
+def export():
+    counter.export_csv()
+    return "OK", 204
+
+@app.route("/get", methods = ["GET"])
+def get():
+    return str(counter.count), 200
+
 
 
 if __name__ == "__main__":
-    server = Flask_server()
-    server.app.run(host="0.0.0.0", port=80, debug=False)
+    app.run(debug = True)
